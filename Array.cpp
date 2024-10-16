5523f07862cb5e62983afdf9c1c66bc687926cb6
GhifariGilbran <mghifarigilbran080506@gmail.com>
2024-10-16 22:10:55 +0700

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int angka[5];
+    int size;
+    int valuebesar;
+    int value;
+
+
+    cout << "Masukkan 5 nilai :" << endl;
+    for(int i = 1; i < 6; ++i){
+
+        cout << "Masukkan nilai ke-" << i << ":";
+        cin >> angka[i];
+    }
+
+    int terbesar = angka[5];
+    for (int i = 1; i < 6; i++) {
+        if (angka[i] > terbesar) {
+            terbesar = angka[i];
+        }
+    }
+    cout << "Nilai tertinggi adalah: " << terbesar << endl;
+
+    cout << endl;
+
+    int cari;
+    cout << "Masukkan angka untuk diperiksa: ";
+    cin >> cari;
+
+    cout << endl;
+
+    bool found = false;
+    for (int i = 1; i < 6; i++) {
+        if (angka[i] == cari) {
+            cout << "nilai " << cari << " ditemukan, di urutan ke " << i << endl;
+            found = true;
+        }
+    }
+
+    if(!found){
+
+        cout << "nilai " << cari << " tidak ditemukan" << endl;
+
+
+    }
+
+
+
+
+    return 0;
+
+}
